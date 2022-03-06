e258d5d48c82f1b27a7ee16ba8bcf33ad2e24e90
JangWoosuk <dntjrdl0517@naver.com>
2022-03-06 20:30:20 +0900

    test Commit!

---
+++ src/test/java/com/example/woosukawsstudy/WoosukAwsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woosukawsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoosukAwsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
